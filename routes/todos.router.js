cb99770efb4c51f4c65501aac0d3064779d4023f
Helena <helena200805@gmail.com>
2025-10-16 10:14:54 +0700

    update

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { getAllTodos, createTodo, getTodoById, updateTodo, deleteTodo, deleteAllTodos } = require('../controllers/todos.controller');
 const { auth } = require('../middleware/auth');
 
-router.get('/', auth, getAllTodos);
-router.post('/', auth, createTodo);
-router.get('/:id', auth, getTodoById);
-router.put('/:id', auth, updateTodo);
-router.delete('/:id', auth, deleteTodo);
-router.delete('/', auth, deleteAllTodos);
+router.get('/', getAllTodos);
+router.post('/', createTodo);
+router.get('/:id', getTodoById);
+router.put('/:id', updateTodo);
+router.delete('/:id', deleteTodo);
+router.delete('/', deleteAllTodos);
 
 module.exports = router;
