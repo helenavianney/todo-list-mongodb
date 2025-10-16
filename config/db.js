3a721d2ed27fa845ad6d73c47a375a4149c9d1ac
Helena <helena200805@gmail.com>
2025-10-16 10:10:24 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const MONGO_URL = process.env.MONGO_URL
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('');
+    await mongoose.connect(MONGO_URL);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
